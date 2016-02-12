0cf38847aa9d46d39cd87549e0f551f7e32529db
Paul KERCKHOVE <pkerckho@e1r5p19.42.fr>
2016-02-12 13:37:09 +0100

    fdf

--- ft_parse.c
+++ ft_parse.c
@@ -6,7 +6,7 @@
 /*   By: pkerckho <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/11 16:27:10 by pkerckho          #+#    #+#             */
-/*   Updated: 2016/02/12 13:06:54 by pkerckho         ###   ########.fr       */
+/*   Updated: 2016/02/12 13:36:55 by pkerckho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 void		ft_parse2(t_env *e, char *file)
 {
 	char	*line;
-	size_t		i;
+	size_t	i;
 
 	i = 0;
 	while ((ft_get_next_line(e->fd, &line) == 1))
@@ -35,9 +35,14 @@ void		ft_parse2(t_env *e, char *file)
 ** In this function we are "parsing", checking if what we are
 ** openning and reading exist (is openned and readable) then we call our
 ** second parsing function (ft_parse2), it will apply our ft_get_next_line
-** function on the data and malloc it.
+** and we use the variable (i) to count till the end and have the size.
+** Then we close the file because we are at then end of it and we re open it
+** to start from the beginning. Now we malloc each start of line of our board
+** because we know the size of the total baord (i) but not the exact size of
+** each line.
 ** Back to our ft_parse, if the ft_get_next_line hasn't failed we use our two
-** variables (i, j) to browse through it and print it (ft_putendl).
+** variables (i, j) to browse through it and print our whole board in line, and
+** we use (ft_putendl) to print it to check.
 */
 
 void		ft_parse(t_env *e, char *file, int argc)

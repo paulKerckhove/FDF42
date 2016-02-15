3446a4e628ef578a05df75b7ddf947206db8d8b6
Paul KERCKHOVE <pkerckho@e1r12p14.42.fr>
2016-02-15 18:36:50 +0100

    fdf

--- ft_parse.c
+++ ft_parse.c
@@ -6,61 +6,105 @@
 /*   By: pkerckho <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/11 16:27:10 by pkerckho          #+#    #+#             */
-/*   Updated: 2016/02/15 11:07:29 by pkerckho         ###   ########.fr       */
+/*   Updated: 2016/02/15 18:20:23 by pkerckho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
-#include "libft/includes/libft.h"
+
+int			ft_split_line(t_env *e)
+{
+	char	*line;
+	int		ret;
+
+	ret = 0;
+	if (get_next_line(e->fd, &line) == 1)
+	{
+		e->line = ft_strsplit(line, ' ');
+		free(line);
+		++ret;
+	}
+	return (ret);
+}
 
 void		ft_parse2(t_env *e, char *file)
 {
-	char	*line;
-	size_t	i;
+	char		*line;
+	int			len;
 
-	i = 0;
-	while ((ft_get_next_line(e->fd, &line) == 1))
+	e->cnt_col = 0;
+	e->cnt_line = 0;
+	while (get_next_line(e->fd, &line) == 1)
 	{
-		i++;
+		++e->cnt_line;
+		len = ft_strlen(line);
 		free(line);
 	}
+	if (e->cnt_line == 0 || len == 0)
+		ft_error("read has failed");
+	e->tab = (int **)ft_memalloc(sizeof(int *) * e->cnt_line);
 	close(e->fd);
 	e->fd = open(file, O_RDONLY);
-	if (!(e->line = (char **)ft_memalloc(sizeof(char *) * (i + 1))))
-		ft_error("the malloc has failed");
-	free(line);
+	ft_split_line(e);
+	while (e->line[e->cnt_col])
+		++e->cnt_col;
+	while (e->cnt_line-- > 0)
+		e->tab[e->cnt_line] = (int *)ft_memalloc(sizeof(int *) * e->cnt_col);
 }
 
-/*
-** In this function we are "parsing", checking if what we are
-** openning and reading exist (is openned and readable) then we call our
-** second parsing function (ft_parse2), it will apply our ft_get_next_line
-** and we use the variable (i) to count till the end and have the size.
-** Then we close the file because we are at then end of it and we re open it
-** to start from the beginning. Now we malloc each start of line of our board
-** because we know the size of the total baord (i) but not the exact size of
-** each line.
-** Back to our ft_parse, if the ft_get_next_line hasn't failed we use our two
-** variables (i, j) to browse through it and print our whole board in line, and
-** we use (ft_putendl) to print it to check.
-*/
+void	ft_freeparse2(t_env *e)
+{
+	int		i;
+
+	i = 0;
+	while (e->line[i])
+	{
+		ft_strdel(&e->line[i]);
+		i++;
+	}
+}
 
 void		ft_parse(t_env *e, char *file)
 {
-	size_t	i;
-	size_t	j;
+	size_t		nmbr_col;
 
-	i = 0;
-	j = -1;
+	nmbr_col = 0;
 	if ((e->fd = open(file, O_RDONLY)) <= 0)
 		ft_error("the open has failed");
 	ft_parse2(e, file);
-	while ((ft_get_next_line(e->fd, &(e->line[i])) == 1))
-		i++;
-	while (++j < i)
+	while (nmbr_col < e->cnt_col)
 	{
-		ft_putendl(e->line[j]);
-		free(e->line[j]);
+		e->tab[0][nmbr_col] = ft_atoi(e->line[nmbr_col]);
+		++nmbr_col;
 	}
-	free(e->line);
+	ft_parse2(e, file);
+	while (ft_split_line(e) == 1)
+	{
+		++e->cnt_line;
+		nmbr_col = 0;
+		while (e->line[nmbr_col])
+		{
+			e->tab[e->cnt_line][nmbr_col] = ft_atoi(e->line[nmbr_col]);
+			++nmbr_col;
+		}
+		if (nmbr_col != e->cnt_col)
+			ft_error("invalid map");
+	}
+
+	int		x;
+	int		y;
+
+	y = 0;
+	while (y < e->cnt_line)
+	{
+		x = 0;
+		while (x < e->cnt_col)
+		{
+			ft_putnbr(e->tab[y][x]);
+			ft_putchar(' ');
+			++x;
+		}
+		ft_putchar('\n');
+		++y;
+	}
 }

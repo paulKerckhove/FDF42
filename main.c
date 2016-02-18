f888f6ea0f1109792fa7f6799bceb8a41e3b1c4a
Paul KERCKHOVE <pkerckho@e1r5p23.42.fr>
2016-02-18 18:25:07 +0100

    fdf

--- main.c
+++ main.c
@@ -6,33 +6,45 @@
 /*   By: pkerckho <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/09 14:04:26 by pkerckho          #+#    #+#             */
-/*   Updated: 2016/02/17 16:28:11 by pkerckho         ###   ########.fr       */
+/*   Updated: 2016/02/18 18:10:27 by pkerckho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 #include <stdio.h>
 
-
-
-void		ft_print(t_env e)
+void		ft_iso(size_t x, size_t y, t_env *e)
 {
-	int		tile;
+	int		prime_x;
+	int		prime_y;
 
-	if (WIN_Y / (e.cnt_line + 1) < WIN_X / (e.cnt_col + 1))
-		tile = WIN_Y / (e.cnt_line + 1);
-	else
-		tile = WIN_X / (e.cnt_col + 1);
-	e.y = (WIN_Y - ((e.cnt_line - 1) * tile)) / 2;
-	while (e.y <= WIN_Y - (WIN_Y - ((e.cnt_line - 1) * tile)) / 2)
+	e->x = y * 20 + x * 20 + (WIN_X / 5 * 2);
+	e->y = x * 20 + x * 20 + (WIN_Y / 5 * 2);
+
+	if (y == 0)
+		prime_y = e->y;
+	if (x == 0)
+		prime_x = e->x;
+	prime_x = e->y;
+	prime_y = e->x;
+}
+
+void		ft_print(t_env *e)
+{
+	size_t		x;
+	size_t		y;
+
+	y = 0;
+	while (y < e->cnt_line)
 	{
-		e.x = (WIN_X - ((e.cnt_col - 1) * tile)) / 2;
-		while (e.x <= WIN_X - (WIN_X - ((e.cnt_col - 1) * tile)) / 2)
+		x = 0;
+		while (x < e->cnt_col)
 		{
-			mlx_pixel_put(e.mlx, e.win, e.x, e.y, e.color);
-			e.x = e.x + tile;
+			ft_iso(x, y, e);
+			mlx_pixel_put(e->mlx, e->win, e->x, e->y, e->color);
+			++x;
 		}
-		e.y = e.y  + tile;
+		++y;
 	}
 }
 
@@ -50,11 +62,11 @@ int		main(int argc, char **argv)
 	if (argc != 2)
 		ft_error("wrong number of arguments");
 	ft_parse(&e, argv[1]);
-	e.color = 0x00FFFF;
+	e.color = 0x0099FF;
 	e.y = WIN_Y / 4;
 	e.mlx = mlx_init();
 	e.win = mlx_new_window(e.mlx, WIN_X, WIN_Y, "mlx42");
-	ft_print(e);
+	ft_print(&e);
 	mlx_key_hook(e.win, ft_keyfunct, &e);
 	mlx_loop(e.mlx);
 	return (0);

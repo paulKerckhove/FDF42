9bd7912b1ed664fd85f5f741668a250e1f99df69
Paul KERCKHOVE <pkerckho@e1r5p17.42.fr>
2016-02-25 18:40:07 +0100

    fdf settings

--- ft_settings.c
+++ ft_settings.c
@@ -6,7 +6,7 @@
 /*   By: pkerckho <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/18 15:44:46 by pkerckho          #+#    #+#             */
-/*   Updated: 2016/02/23 14:52:01 by pkerckho         ###   ########.fr       */
+/*   Updated: 2016/02/25 18:39:55 by pkerckho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,21 @@
 
 void		ft_settings(void)
 {
-	ft_putstr("\n\n=======================================================");
-	ft_putstr("\n                   FDF 42 by pkerckho                  \n");
-	ft_putstr("=======================================================\n\n");
-	ft_putstr("                      ------------                    |  \n");
-	ft_putstr("                      | Controls |                     | \n");
-	ft_putstr("                      ------------                    |\n\n");
-	ft_putstr("#########################################################\n");
-	ft_putstr("#   move up              ||      up                     #\n");
-	ft_putstr("#   move down            ||     down                    #\n");
-	ft_putstr("#   move left            ||     left                    #\n");
-	ft_putstr("#   move right           ||     right                   #\n");
-	ft_putstr("#   zoom in              ||      +                      #\n");
-	ft_putstr("#   zoom out             ||      -                      #\n");
-	ft_putstr("#########################################################\n");
+	ft_putstr("\n\n  |####################################################|");
+	ft_putstr("\n  |                FDF 42 by pkerckho                  |\n");
+	ft_putstr("  |====================================================|\n");
+	ft_putstr("  |                   #----------#                     |\n");
+	ft_putstr("  |                   | Controls |                     |\n");
+	ft_putstr("  |                   #----------#                     |\n");
+	ft_putstr("  |####################################################|\n");
+	ft_putstr("  |   move up              ||           up             |\n");
+	ft_putstr("  |   move down            ||          down            |\n");
+	ft_putstr("  |   move left            ||          left            |\n");
+	ft_putstr("  |   move right           ||          right           |\n");
+	ft_putstr("  |   zoom in              ||            +             |\n");
+	ft_putstr("  |   zoom out             ||            -             |\n");
+	ft_putstr("  |   epileptic mode       ||          page up         |\n");
+	ft_putstr("  |   exit program         ||           esc            |\n")
+	ft_putstr("  |####################################################|\n");
 }
 

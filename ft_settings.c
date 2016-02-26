aec25fdb9b1d15b2e716c6b03ac72db42848b523
Paul KERCKHOVE <pkerckho@e1r5p17.42.fr>
2016-02-26 17:02:29 +0100

    fdf

--- ft_settings.c
+++ ft_settings.c
@@ -6,14 +6,36 @@
 /*   By: pkerckho <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/18 15:44:46 by pkerckho          #+#    #+#             */
-/*   Updated: 2016/02/25 18:39:55 by pkerckho         ###   ########.fr       */
+/*   Updated: 2016/02/26 16:50:46 by pkerckho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void		ft_settings(void)
+void		ft_settings(t_env e)
 {
+	int		i_y;
+
+	i_y = 0;
+	mlx_string_put(e.mlx, e.win, 0, i_y, 0xccccff, I_SETINGS);
+	mlx_string_put(e.mlx, e.win, 0, i_y + 16, 0xccccff, I_KEY_UP);
+	mlx_string_put(e.mlx, e.win, 0, i_y + 32, 0xccccff, I_KEY_DOWN);
+	mlx_string_put(e.mlx, e.win, 0, i_y + 48, 0xccccff, I_KEY_LEFT);
+	mlx_string_put(e.mlx, e.win, 0, i_y + 64, 0xccccff, I_KEY_RIGHT);
+	mlx_string_put(e.mlx, e.win, 0, i_y + 80, 0xccccff, I_KEY_PLUS);
+	mlx_string_put(e.mlx, e.win, 0, i_y + 96, 0xccccff, I_KEY_MINUS);
+	mlx_string_put(e.mlx, e.win, 0, i_y + 112, 0xccccff, I_PAGE_UP);
+	mlx_string_put(e.mlx, e.win, 0, i_y + 128, 0xccccff, I_PAGE_DOWN);
+	mlx_string_put(e.mlx, e.win, 0, i_y + 144, 0xccccff, I_STAR);
+	mlx_string_put(e.mlx, e.win, 0, i_y + 160, 0xccccff, I_SLASH);
+	mlx_string_put(e.mlx, e.win, 0, i_y + 176, 0xccccff, I_RESET);
+	mlx_string_put(e.mlx, e.win, 0, i_y + 192, 0xccccff, I_ESC);
+
+
+
+
+
+//{
 	ft_putstr("\n\n  |####################################################|");
 	ft_putstr("\n  |                FDF 42 by pkerckho                  |\n");
 	ft_putstr("  |====================================================|\n");
@@ -28,7 +50,8 @@ void		ft_settings(void)
 	ft_putstr("  |   zoom in              ||            +             |\n");
 	ft_putstr("  |   zoom out             ||            -             |\n");
 	ft_putstr("  |   epileptic mode       ||          page up         |\n");
-	ft_putstr("  |   exit program         ||           esc            |\n")
+	ft_putstr("  |   exit program         ||           esc            |\n");
 	ft_putstr("  |####################################################|\n");
+	//
 }
 

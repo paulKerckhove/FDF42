9945227f4861c543d47e9d2029454982361116f2
Paul KERCKHOVE <pkerckho@e1r5p17.42.fr>
2016-03-02 15:55:00 +0100

    fdf

--- ft_draw.c
+++ ft_draw.c
@@ -6,11 +6,21 @@
 /*   By: pkerckho <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/22 15:42:06 by pkerckho          #+#    #+#             */
-/*   Updated: 2016/02/25 13:20:39 by pkerckho         ###   ########.fr       */
+/*   Updated: 2016/03/02 11:45:07 by pkerckho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+
+void		ft_put_pixel(t_env *e, int x, int y, int color)
+{
+	int		*i;
+
+	if (y >= WIN_Y || x >= WIN_X || x < 0 || y < 0)
+		return ;
+	i = (int *)&e->imc[(y * e->imlen) + (x * (e->bpp / 8))];
+	*i = color;
+}
 
 void		ft_draw(int x2, int y2, t_env *e)
 {
@@ -23,7 +33,7 @@ void		ft_draw(int x2, int y2, t_env *e)
 	e->error = (e->dx > e->dy ? e->dx : -(e->dy)) / 2;
 	while (!(e->tmpy == y2 && e->tmpx == x2))
 	{
-		mlx_pixel_put(e->mlx, e->win, e->tmpx, e->tmpy, e->color);
+		ft_put_pixel(e, e->tmpx, e->tmpy, e->color);
 		e->tmp_error = e->error;
 		if (e->tmp_error > -(e->dx))
 		{
